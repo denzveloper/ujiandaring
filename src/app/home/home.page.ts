c417490877dbdb5da06c8b7e3c0ca0a0fbe73c77
DENZVELOPER - DzEN/DzEN <dandyoctavian@gmail.com>
2019-07-04 01:59:16 +0700

    update style

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { NavController, MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(
+    private storage: Storage,
+    private menu: MenuController,
+    private nav: NavController
+  ) {
+    this.menu.enable(true);
+  }
 
+  ngOnInit(){
+    this.storage.get('user').then(user => {
+      if(user == null){
+        this.nav.navigateRoot('/login');
+      }
+    });
+  }
+
+  
 }
