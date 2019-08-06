b9e19049ea02e20dd6e3fc59a75f192ef10ba763
DENZVELOPER - DzEN/DzEN <dandyoctavian@gmail.com>
2019-08-06 18:15:48 +0700

    Add Pass value

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -21,23 +21,27 @@ export class AuthService {
     return this.http.post("-address-", form).pipe(timeout(9000));
   */
 
+  //Test Output
+  // return this.http.post("http://localhost/htdocs/api/apiup.php", form);
+
   dologin(form){
     //Harap ganti address
-    // return this.http.post("http://localhost/htdocs/api/api.php", form).pipe(timeout(10000));;
-    // return this.http.post("http://localhost/htdocs/api/apiup.php", form);
-    return this.http.post("http://srdoni.1603046.domainon.top/API/api.php", form).pipe(timeout(30000));
+    // return this.http.post("http://localhost/htdocs/api/api.php", form).pipe(timeout(10000)); //localhost
+    return this.http.post("http://d0ce5af4.ngrok.io/htdocs/api/api.php", form).pipe(timeout(10000)); //server dynamic
+    // return this.http.post("http://srdoni.1603046.domainon.top/API/api.php", form).pipe(timeout(30000)); //Server
   }
 
   doupload(form){
     //Harap ganti address
-    // return this.http.post("http://localhost/htdocs/api/aps.php", form);
-    return this.http.post("http://srdoni.1603046.domainon.top/API/aps.php", form).pipe(timeout(30000));
+    // return this.http.post("http://localhost/htdocs/api/aps.php", form).pipe(timeout(10000)); //Localhost
+    return this.http.post("http://d0ce5af4.ngrok.io/htdocs/api/aps.php", form).pipe(timeout(10000)); //server dynamic
+    // return this.http.post("http://srdoni.1603046.domainon.top/API/aps.php", form).pipe(timeout(30000)); //Server
   }
 
   doupasw(form){
     //Harap ganti address
-    return this.http.post("http://localhost/htdocs/api/apiup.php", form);
-    // return this.http.post("http://srdoni.1603046.domainon.top/API/apiup.php", form).pipe(timeout(10000));
+    return this.http.post("http://localhost/htdocs/api/apiup.php", form); //Localhost
+    // return this.http.post("http://srdoni.1603046.domainon.top/API/apiup.php", form).pipe(timeout(10000)); //Server
   }
 
 }
